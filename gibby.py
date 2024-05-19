8903638b8b09dcce356622020edf79a4380a669b
Kairi Tanaka <64274901+kairitanaka@users.noreply.github.com>
2024-05-19 10:49:05 -0700

    Update gibby.py

--- gibby.py
+++ gibby.py
@@ -1,4 +1,4 @@
-import sys 
+import sys
 import random
 from random import choices
 
@@ -7,29 +7,29 @@ def gibbs_sampler(dna: list[str], k: int, n: int) -> list[str]:
     best_motifs = []
     best_score = float('inf')
     t = len(dna)
-    #what we are doing here: 
-    #we want to initially randomly choose some kmers. We ignore a motif randomly. We find the profile of all the motifs except the ignored one and choose the best probability kmer 
+    #what we are doing here:
+    #we want to initially randomly choose some kmers. We ignore a motif randomly. We find the profile of all the motifs except the ignored one and choose the best probability kmer
     #from the ignored motif by randomly choosing based off of the probaility. Then do a score comparison then repeat
 
     for _ in range(1000):
         motifs = randomKmers(dna, k) # random Kmer Patterns and their start positions
-        current_best_motifs = motifs[:] # copy motifs for intialization 
-        current_best_score = Score(current_best_motifs,k) # score the motifs and intialize the best score 
+        current_best_motifs = motifs[:] # copy motifs for intialization
+        current_best_score = Score(current_best_motifs,k) # score the motifs and intialize the best score
         for _ in range(n):
-            ignoredKmerIndex = chooseRandomKmerIndex(t) #choose random i 
+            ignoredKmerIndex = chooseRandomKmerIndex(dna) #choose random i
             profile = Profile(current_best_motifs[:ignoredKmerIndex] + current_best_motifs[ignoredKmerIndex+1:], k) # make a profile from the kmers excluding the ignored Kmer
             motifProbability = Motifs(profile, dna[ignoredKmerIndex], k) #find motif probabilty from the ignoredDNA
-            probableKmers = list(motifProbability.keys()) # kmers in ignoredDNA as they are the keys in the profile 
+            probableKmers = list(motifProbability.keys()) # kmers in ignoredDNA as they are the keys in the profile
             #print(probableKmers)
-            probabilities = list(motifProbability.values()) # probabilities of those kmers using profile 
+            probabilities = list(motifProbability.values()) # probabilities of those kmers using profile
             chosenKmer = choices(probableKmers, probabilities) # choose a kmer using the probabilities
-            chosenKmer = "".join(chosenKmer) #join because it comes out in list format 
-            current_best_motifs[ignoredKmerIndex] = chosenKmer #swap out the ignored motif with the chosenKmer 
-            score = Score(current_best_motifs,k) 
-            if score < current_best_score: # score comparison 
-              current_best_score = score #If the score is better replace current_best_score 
-              if score < best_score:
-                best_motifs = current_best_motifs[:] #replace best as well for both. Leave this because it is the final answer 
+            chosenKmer = "".join(chosenKmer) #join because it comes out in list format
+            current_best_motifs[ignoredKmerIndex] = chosenKmer #swap out the ignored motif with the chosenKmer
+            score = Score(current_best_motifs,k)
+            if score < current_best_score: # score comparison
+                current_best_score = score #If the score is better replace current_best_score
+            if score < best_score:
+                best_motifs = current_best_motifs[:] #replace best as well for both. Leave this because it is the final answer
                 best_score = score
     return best_motifs
     pass
@@ -43,9 +43,9 @@ def randomKmers(dna: list[str], k: int) -> list:
             randomK.append(pattern)
     return randomK
 
-def chooseRandomKmerIndex(dna) -> int:
+def chooseRandomKmerIndex(dna:list) -> int:
     t = len(dna)
-    index = random.randint(0,t-1) #choose a random index 
+    index = random.randint(0,t-1) #choose a random index
     return index
     pass
 
@@ -56,22 +56,22 @@ def Profile (randomKmers: list, k: int):
             base = string[i]
             profile[i][base] += 1 # add one to the nucleotide count based on the letter
     for dicts in profile:
-        total = sum(dicts.values()) #sum so that divide by the total to get probability 
+        total = sum(dicts.values()) #sum so that divide by the total to get probability
         for nucleotide in dicts:
-            dicts[nucleotide] /= total #probability 
+            dicts[nucleotide] /= total #probability
     return profile
     pass
 
 
 def Motifs (profile, string, k) -> dict:
     motifProbability = {}
-    for i in range(len(string) - k + 1): 
-        kmer = string[i:i+k] #set window 
+    for i in range(len(string) - k + 1):
+        kmer = string[i:i+k] #set window
         probability = 1
         for j in range(k):
             base = kmer[j] #base at j
             probability *= profile[j][base] #probability at position j given the base
-        motifProbability[kmer] = probability #put kmer and probability in dict so that we can split later for randomly "choosing" given probaility 
+        motifProbability[kmer] = probability #put kmer and probability in dict so that we can split later for randomly "choosing" given probaility
     return motifProbability
     pass
 
@@ -79,14 +79,14 @@ def Motifs (profile, string, k) -> dict:
 def Score (mostProbable: list, k: int):
     score = [{'A': 0, 'C': 0 , 'G': 0, 'T': 0} for _ in range(k)]
     for i in range(k):
-        for string in mostProbable: #for every kmer in mostProbable 
+        for string in mostProbable: #for every kmer in mostProbable
             base = string[i] #take the base
-            score[i][base] += 1 #this makes the count for each nucleotide in the i-th position 
+            score[i][base] += 1 #this makes the count for each nucleotide in the i-th position
     sumList = []
     for i in range(k):
-        dicts = score[i] #all the dictionaries in the score 
-        mostFreq = max(dicts.values()) #most frequent base that is seen in that position 
-        total = len(mostProbable) #total is the number of how many motifs there are 
+        dicts = score[i] #all the dictionaries in the score
+        mostFreq = max(dicts.values()) #most frequent base that is seen in that position
+        total = len(mostProbable) #total is the number of how many motifs there are
         indexScore = total - mostFreq #total - most frequent base leads to the indexScore
         sumList.append(indexScore)
     return sum(sumList) #sum it all up
