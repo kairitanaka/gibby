ee092c3befa3dfb4bb1335debc24d0de2f934a5b
Joseph Hwang <86521451+JoeHwa@users.noreply.github.com>
2024-05-26 16:17:43 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(
     install_requires=[
         'biopython',
         'numpy',
-        'seqlogo'
+        'seqlogo',
+        'tqdm'
     ],
     entry_points={
         'console_scripts': [
